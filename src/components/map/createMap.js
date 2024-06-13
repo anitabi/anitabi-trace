0821ec7bb296745a49a0f739ac9addad66433323
itorr-i7 <592668024@qq.com>
2024-06-13 23:19:01 +0800

    信息更新

--- src/components/map/createMap.js
+++ src/components/map/createMap.js
@@ -3,7 +3,7 @@ import mapboxGl from "mapbox-gl";
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 
-import { streetsStyle } from './streetsStyle.js';
+import { streetsStyle as style } from './streetsStyle.js';
 let localFontFamily = "'system-ui', sans-serif"; // 使用本地字体
 
 // 判断是 iOS MacOS
@@ -11,14 +11,15 @@ if (/(iPhone|iPad|iPod|iOS|Mac OS X)/i.test(navigator.userAgent)) {
 	localFontFamily = "'PingFang SC', sans-serif";
 }
 
+// 临时token 限制了 origin
+const accessToken = 'pk.eyJ1IjoiaXRvcnIiLCJhIjoiY2x4Y2trYzNsMjJjYTJqcjAxM3hqdXB6OSJ9.cyCsL-wkCMZp_mKLnmKp7Q';
+
 
 export const createMap = config=>{
 	const map = new mapboxGl.Map({
 		...config,
-		style: streetsStyle,
-
-		// 临时token 限制了 origin
-		accessToken: 'pk.eyJ1IjoiaXRvcnIiLCJhIjoiY2x4Y2trYzNsMjJjYTJqcjAxM3hqdXB6OSJ9.cyCsL-wkCMZp_mKLnmKp7Q',
+		style,
+		accessToken,
 		worldview: 'CN',
 		attributionControl: false,
 		localFontFamily,
