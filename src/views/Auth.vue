c4480dab1c3122cedbfd78f3b33db71b602ad407
Hydrostic <tpzhuchenghao@gmail.com>
2026-02-15 15:45:53 +0800

    feat(auth): Add nickname validation

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -14,6 +14,12 @@
             <span class="underline-text relative">已注册？直接登录</span>
         </button>
     </div>
+    <div class="absolute bottom-[10vh] w-full flex justify-center">
+        <div class="relative before:bg-black/50 before:w-full before:h-full before:absolute before:inset-0
+        px-[29px] py-[18px] rounded-[10px] before:blur-[12px] max-w-[336px]" v-if="message !== ''">
+            <span class="text-normal z-[calc(var(--index)+1)] relative">{{ message }}</span>
+        </div>
+    </div>
 </template>
 
 
@@ -26,8 +32,44 @@ import { css as ssoFontCss } from '../assets/fonts/SmileySans-Oblique-2.ttf?subs
 const gameStore = useGameStore();
 const userStore = useUserStore();
 const nickname = ref('');
+const message = ref('');
+const showMsg = (msg: string) => {
+    message.value = msg;
+    setTimeout(() => {
+        message.value = '';
+    }, 3000);
+};
+const isCJKorEnglish = (char: string) => {
+  const code = char.codePointAt(0);
+  if (!code) return false;
+  return (
+    // English A-Z a-z
+    (code >= 0x41 && code <= 0x5A) ||
+    (code >= 0x61 && code <= 0x7A) ||
 
+    // Hiragana
+    (code >= 0x3040 && code <= 0x309F) ||
+
+    // Katakana
+    (code >= 0x30A0 && code <= 0x30FF) ||
+    (code >= 0xFF65 && code <= 0xFF9F) ||
+
+    // CJK Unified Ideographs (常用汉字范围)
+    (code >= 0x4E00 && code <= 0x9FFF) ||
+    (code >= 0x3400 && code <= 0x4DBF)
+  );
+}
 const handleSubmit = () => {
+    if (nickname.value.length < 2 || nickname.value.length > 10) {
+        showMsg('昵称长度应为2-10位');
+        return;
+    }
+    for (const char of nickname.value) {
+        if (!isCJKorEnglish(char)) {
+            showMsg('昵称只能包含中文、英文字符');
+            return;
+        }
+    }
     userStore.setNickname(nickname.value);
     gameStore.game.state.next();
 };
